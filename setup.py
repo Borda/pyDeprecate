6f87976d6db678bc2c7ebc0008d8a7af87fa61b3
Jirka Borovec <jirka.borovec@seznam.cz>
2021-03-21 08:32:51 +0100

    add support for py2.7

--- setup.py
+++ setup.py
@@ -63,6 +63,8 @@ setup(
         'Operating System :: OS Independent',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
