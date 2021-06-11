e89b8c2470ae0e717107e106e15ebcc8dabd01c9
Jirka <jirka.borovec@seznam.cz>
2021-06-11 11:45:24 +0200

    setup: status Beta

--- setup.py
+++ setup.py
@@ -49,13 +49,14 @@ setup(
     install_requires=[],
     project_urls={
         "Source Code": deprecate.__source_code__,
+        "Home page": deprecate.__homepage__,
     },
     classifiers=[
         'Environment :: Console',
         'Natural Language :: English',
         # How mature is this project? Common values are
         #   3 - Alpha, 4 - Beta, 5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         # Pick your license as you wish
