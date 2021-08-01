a7d1b460c93a75a0e5f341ef2195d65075f957d0
Jirka Borovec <Borda@users.noreply.github.com>
2021-08-01 15:25:25 +0200

    CI: use mdformat & yesqa (#13)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- deprecate/__init__.py
+++ deprecate/__init__.py
@@ -12,5 +12,5 @@ __license__ = 'MIT'
 _PATH_PACKAGE = os.path.realpath(os.path.dirname(__file__))
 _PATH_PROJECT = os.path.dirname(_PATH_PACKAGE)
 
-from deprecate.deprecation import deprecated  # noqa: F401 E402
-from deprecate.utils import void  # noqa: F401 E402
+from deprecate.deprecation import deprecated  # noqa: E402, F401
+from deprecate.utils import void  # noqa: E402, F401
