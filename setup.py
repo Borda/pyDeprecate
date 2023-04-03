0eac9fd0c498ae4fc662ab91582046de1d4d2050
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-04-03 19:16:47 +0200

    move to src/ (#29)
    * move to src/
    * CI update
    * about
    * py3.7
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 """Copyright (C) 2020-2021 Jiri Borovec <...>"""
 import os
+from importlib.util import module_from_spec, spec_from_file_location
 
 # Always prefer setuptools over distutils
 from setuptools import find_packages, setup
 
-import deprecate
+_PATH_ROOT = os.path.realpath(os.path.dirname(__file__))
+_PATH_SOURCE = os.path.join(_PATH_ROOT, "src")
 
-# https://packaging.python.org/guides/single-sourcing-package-version/
-# http://blog.ionelmc.ro/2014/05/25/python-packaging/
 
-_PATH_ROOT = os.path.dirname(__file__)
+def _load_py_module(fname: str, pkg: str = "deprecate"):
+    spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_SOURCE, pkg, fname))
+    py = module_from_spec(spec)
+    spec.loader.exec_module(py)
+    return py
+
+
+ABOUT = _load_py_module("__about__.py")
 
 
 def _load_long_description(path_dir: str, version: str) -> str:
@@ -30,14 +37,15 @@ def _load_long_description(path_dir: str, version: str) -> str:
 # engineer specific practices
 setup(
     name="pyDeprecate",
-    version=deprecate.__version__,
-    description=deprecate.__docs__,
-    author=deprecate.__author__,
-    author_email=deprecate.__author_email__,
-    url=deprecate.__homepage__,
-    license=deprecate.__license__,
-    packages=find_packages(exclude=["tests", "docs"]),
-    long_description=_load_long_description(_PATH_ROOT, version=deprecate.__version__),
+    version=ABOUT.__version__,
+    description=ABOUT.__docs__,
+    author=ABOUT.__author__,
+    author_email=ABOUT.__author_email__,
+    url=ABOUT.__homepage__,
+    license=ABOUT.__license__,
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    long_description=_load_long_description(_PATH_ROOT, version=ABOUT.__version__),
     long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
@@ -45,7 +53,7 @@ setup(
     python_requires=">=3.6",
     setup_requires=[],
     install_requires=[],
-    project_urls={"Source Code": deprecate.__source_code__, "Home page": deprecate.__homepage__},
+    project_urls={"Source Code": ABOUT.__source_code__, "Home page": ABOUT.__homepage__},
     classifiers=[
         "Environment :: Console",
         "Natural Language :: English",
@@ -60,10 +68,10 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
--- src/deprecate/__init__.py
+++ src/deprecate/__init__.py
@@ -1,14 +1,6 @@
 """Copyright (C) 2020-2021 Jiri Borovec <...>"""
 import os
 
-__version__ = "0.3.2"
-__docs__ = "Deprecation tooling"
-__author__ = "Jiri Borovec"
-__author_email__ = "jiri.borovec@fel.cvut.cz"
-__homepage__ = "https://borda.github.io/pyDeprecate"
-__source_code__ = "https://github.com/Borda/pyDeprecate"
-__license__ = "Apache-2.0"
-
 _PATH_PACKAGE = os.path.realpath(os.path.dirname(__file__))
 _PATH_PROJECT = os.path.dirname(_PATH_PACKAGE)
 
