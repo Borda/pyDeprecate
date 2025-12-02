4697b3cc80e948d81a4f2116851b5814e9c03596
jirka <jirka.borovec@seznam.cz>
2025-12-02 11:18:35 +0100

    -vv

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -56,8 +56,8 @@ __all__ = [
 def test_deprecated_func_docstring() -> None:
     """Test that deprecated functions have deprecation warning in their docstring."""
     assert old_function.__doc__ == (
-        "An old function that is deprecated."
-        "\n\n.. deprecated:: 0.1\n"
+        "An old function that is deprecated.\n\n"
+        ".. deprecated:: 0.1\n"
         "   Will be removed in 0.3.\n"
         "   Use `tests.test_docs.new_function` instead.\n"
     )
@@ -71,8 +71,8 @@ def test_deprecated_func_docstring_plain() -> None:
 def test_deprecated_class_docstring() -> None:
     """Test that deprecated classes have deprecation warning in their __init__ docstring."""
     assert OldClass.__init__.__doc__ == (
-        "Initialize the old class."
-        "\n\n.. deprecated:: 0.2\n"
+        "Initialize the old class.\n\n"
+        ".. deprecated:: 0.2\n"
         "   Will be removed in 0.4.\n"
         "   Use `tests.test_docs.NewClass` instead.\n"
     )
